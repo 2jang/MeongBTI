fc7b9035ef292a7fbbe39398f8edf97b778ebaa2
2jang <jakeecc@naver.com>
2024-07-31 16:30:48 +0900

    2차 프

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+from static.dbti.DBTIDB_conn import save_result
+
 app = Flask(__name__)
+CORS(app)  # CORS 설정
+
+# MySQL 설정
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'easygymdb'
+
+mysql = MySQL(app)
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
+
+
+@app.route('/saveResult', methods=['POST'])
+def handle_save_result():
+    return save_result(request, mysql)
+
 
 if __name__ == '__main__':
     app.run()
